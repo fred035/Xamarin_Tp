400d25b99f116c630c95f59652dc2bcde849fdb9
harckline <antoinecronier1@gmail.com>
2020-03-31 13:44:55 +0200

    Navigation Service

--- ENI-Xamarin-30032020/ENI-Xamarin-30032020/App.xaml.cs
+++ ENI-Xamarin-30032020/ENI-Xamarin-30032020/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ENI_Xamarin_30032020.Configurations;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +9,22 @@ namespace ENI_Xamarin_30032020
 {
     public partial class App : Application
     {
+        private static ViewModelLocator _locator;
+        public static ViewModelLocator Locator
+        {
+            get
+            {
+                return _locator ?? (_locator = new ViewModelLocator());
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            var firstPage = new NavigationPage(new MainPage());
+
+            MainPage = firstPage;
         }
 
         protected override void OnStart()
