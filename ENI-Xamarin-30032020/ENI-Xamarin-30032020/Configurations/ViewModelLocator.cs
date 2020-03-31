00596acf927c8f1a36a28ddf32f5f327944780ce
harckline <antoinecronier1@gmail.com>
2020-03-31 13:45:20 +0200

    Navigation Service

--- ENI-Xamarin-30032020/ENI-Xamarin-30032020/Configurations/ViewModelLocator.cs
+++ ENI-Xamarin-30032020/ENI-Xamarin-30032020/Configurations/ViewModelLocator.cs
@@ -1,6 +1,7 @@
 ﻿using CommonServiceLocator;
 using ENI_Xamarin_30032020.ViewModels;
 using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +10,22 @@ namespace ENI_Xamarin_30032020.Configurations
 {
     public class ViewModelLocator
     {
+        public enum Pages
+        {
+            MainPage
+        }
+
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<INavigationService>(() =>
+            {
+                var navigationService = new NavigationService();
+                navigationService.Configure(Pages.MainPage.ToString(), typeof(MainPage));
+                return navigationService;
+            });
+
             SimpleIoc.Default.Register<MainPageViewModel>();
         }
 
