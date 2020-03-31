a6fe36db1f8b72482041fa266cc2571eac7383fb
harckline <antoinecronier1@gmail.com>
2020-03-31 13:26:47 +0200

    gen apk + mvvmlight

---
+++ ENI-Xamarin-30032020/ENI-Xamarin-30032020/Configurations/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using CommonServiceLocator;
+using ENI_Xamarin_30032020.ViewModels;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENI_Xamarin_30032020.Configurations
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+
+
+        public MainPageViewModel MainViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainPageViewModel>();
+            }
+        }
+    }
+}
---
+++ ENI-Xamarin-30032020/ENI-Xamarin-30032020/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENI_Xamarin_30032020.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+    }
+}
