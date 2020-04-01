3c770d2588032c17a5317eaaa9535f73da9a8cec
harckline <antoinecronier1@gmail.com>
2020-04-01 15:09:16 +0200

    prepare TP05

---
+++ Xamarin-Module8-TP05/Xamarin-Module8-TP05/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin_Module8_TP05.Entities;
+using Xamarin_Module8_TP05.Services;
+
+namespace Xamarin_Module8_TP05
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private ITwitterService twitterService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.btnConnexion.Clicked += BtnConnexion_Clicked;
+            this.errorLabel.IsVisible = false;
+            this.tweetList.IsVisible = false;
+            this.twitterService = new TwitterServiceImpl();
+        }
+
+        private void BtnConnexion_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("btn clicked");
+            String login = this.identifiant.Text;
+            String password = this.motDePasse.Text;
+            Boolean isRemind = this.memorise.IsToggled;
+
+            String errors = this.twitterService.Authenticate(new User() { Login = login, Password = password });
+
+            if (!String.IsNullOrEmpty(errors))
+            {
+                this.errorLabel.Text = errors;
+                this.errorLabel.IsVisible = true;
+            }
+            else
+            {
+                this.errorLabel.Text = "";
+                this.errorLabel.IsVisible = false;
+                this.connectionForm.IsVisible = false;
+                this.tweetList.IsVisible = true;
+            }
+        }
+    }
+}
---
+++ Xamarin-Module8-TP05/Xamarin-Module8-TP05/Services/ITwitterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin_Module8_TP05.Entities;
+
+namespace Xamarin_Module8_TP05.Services
+{
+    public interface ITwitterService
+    {
+        String Authenticate(User user);
+        List<Tweet> Tweets { get; }
+    }
+}
