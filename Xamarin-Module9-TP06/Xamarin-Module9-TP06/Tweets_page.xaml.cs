d2a19163b7d64c45e0106b04a55a5cc5dc93cfff
LOCHERFrederic <LOCHERFrederic@DESKTOP-6RG138O>
2020-04-02 14:20:35 +0200

    add ListView

--- Xamarin-Module9-TP06/Xamarin-Module9-TP06/Tweets_page.xaml.cs
+++ Xamarin-Module9-TP06/Xamarin-Module9-TP06/Tweets_page.xaml.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin_Module9_TP06.Entities;
 
 namespace Xamarin_Module9_TP06
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Tweets_page : ContentPage
     {
+        ObservableCollection<Tweet> tweets = new ObservableCollection<Tweet>();
         public Tweets_page()
         {
             InitializeComponent();
+            this.tweets.Add(new Tweet { 
+                User = new User { Login = "Antoine", Password = "1234" }, 
+                Data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod convallis dolor.Quisque ultricies porttitor nunc venenatis facilisis.", 
+                CreatedAt= new DateTime() });
+            this.ListeTweets.ItemsSource = this.tweets;
         }
     }
 }
